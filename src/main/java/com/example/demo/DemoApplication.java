e1c041d2c5b18565302ef150c014f5db8259d609
YeoKyuLi <kyuli.yeo@gmail.com>
2020-06-07 18:56:54 +0900

    MongoDB

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,18 +1,15 @@
 package com.example.demo;
 
-import com.example.demo.accessingmongodb.PersonRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+
+
 
 @Configuration
-@Import(PersonRepository.class)
 @SpringBootApplication
 public class DemoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-
 }
